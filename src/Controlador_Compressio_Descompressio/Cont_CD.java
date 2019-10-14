661d07924fb5878c99de9562ca0942eb0344fd71
asocar <danielllisto@gmail.com>
2019-10-14 19:00:08 +0200

    Nada

--- src/Controlador_Compressio_Descompressio/Cont_CD.java
+++ src/Controlador_Compressio_Descompressio/Cont_CD.java
@@ -1,5 +1,79 @@
 package Controlador_Compressio_Descompressio;
 
+import java.io.*;
+import java.util.List;
+import Algoritmes.*;
+
 public class Cont_CD {
 
+    public class Item {
+        private String path;
+        private int best_metod;
+        private List<Integer> Cod;
+
+        public Item () {
+            path = "/home/asocar/Desktop/Ejemplo.txt";
+            best_metod = 3;
+            Cod.add(3);
+        }
+
+        public String getpath() { return this.path; }
+
+        public int gethmetod() { return this.best_metod; }
+
+        public List<Integer> getcode() { return this.Cod; }
+    }
+
+    public void compressio_descompressio (Item I, int id, String path_dest, boolean comprimir) throws IOException {
+        String path = I.getpath();
+        if (path_dest == "") path_dest = path;
+        if (id == 0) id = I.gethmetod();
+        List<Integer> L;
+        String S;
+        File file = new File(path);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        if (comprimir) {
+            switch (id) {
+                case 1:
+                    LZ78 L8 = new LZ78();
+                    break;
+
+                case 2:
+                    LZSS LS = new LZSS();
+                    break;
+
+                case 3:
+                    LZW LW = new LZW();
+                    L = LW.compress(br);
+                    break;
+
+                case 4:
+                    JPEG JG = new JPEG();
+                    break;
+            }
+        } else {
+            switch (id) {
+                case 1:
+                    LZ78 L8 = new LZ78();
+                    break;
+
+                case 2:
+                    LZSS LS = new LZSS();
+                    break;
+
+                case 3:
+                    LZW LW = new LZW();
+                    //S = LW.descomprimir(br);
+                    break;
+
+                case 4:
+                    JPEG JG = new JPEG();
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
