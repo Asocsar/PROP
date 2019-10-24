11d6791182e1c3ff8e0c1c3343500601676b0f78
asocar <danielllisto@gmail.com>
2019-10-24 21:49:15 +0200

    R

--- src/Algoritmes/JPEG.java
+++ src/Algoritmes/JPEG.java
@@ -1,35 +1,38 @@
 package Algoritmes;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
+import java.io.*;
 
 
 public class JPEG {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) {
 
-        File infile = new File("/home/maller/Downloads/cluet.ppm");
-        File outfile = new File("/home/maller/Downloads/compresset.ppm");
+        try {
 
-        long size = infile.length(); //Tamany del fitxer per posar-lo al buffer
-        byte[] buffer = new byte[(int)size];
+            File infile = new File("/home/maller/Downloads/cluet.ppm");
+            File outfile = new File("/home/maller/Downloads/compresset.ppm");
 
-        InputStream is = new FileInputStream(infile);
-        is.read(buffer, 0, buffer.length);
-        is.close();
+            long size = infile.length(); //Tamany del fitxer per posar-lo al buffer
+            byte[] buffer = new byte[(int) size];
 
-        //Comprimir a buffer
+            InputStream is = new FileInputStream(infile);
+            is.read(buffer, 0, buffer.length);
+            is.close();
 
-        //Descomprimir a buffer
+            //Comprimir a buffer
 
-        OutputStream os = new FileOutputStream(outfile);
-        os.write(buffer, 0, buffer.length);
-        os.close();
+            //Descomprimir a buffer
 
-        System.out.println("JPEG acabat amb èxit.");
+            OutputStream os = new FileOutputStream(outfile);
+            os.write(buffer, 0, buffer.length);
+            os.close();
+
+            System.out.println("JPEG acabat amb èxit.");
+        }
+
+        catch (FileNotFoundException e) {
+            System.out.println("El fichero no ha sido encontrado");
+        } catch (IOException e) {
+            System.out.println("Error en la entrada saldia");
+        }
     }
 }
