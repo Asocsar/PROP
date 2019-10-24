e813e583fb161f25cab2e6bb8ad09a2c133807ca
joanmaller <joanmaller@gmail.com>
2019-10-24 21:38:10 +0200

    Lectura_escriptura

--- src/Algoritmes/JPEG.java
+++ src/Algoritmes/JPEG.java
@@ -1,7 +1,35 @@
 package Algoritmes;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+
 public class JPEG {
-    public static void main(String[] args) {
-    int n = 4;
+    public static void main(String[] args) throws Exception{
+
+        File infile = new File("/home/maller/Downloads/cluet.ppm");
+        File outfile = new File("/home/maller/Downloads/compresset.ppm");
+
+        long size = infile.length(); //Tamany del fitxer per posar-lo al buffer
+        byte[] buffer = new byte[(int)size];
+
+        InputStream is = new FileInputStream(infile);
+        is.read(buffer, 0, buffer.length);
+        is.close();
+
+        //Comprimir a buffer
+
+        //Descomprimir a buffer
+
+        OutputStream os = new FileOutputStream(outfile);
+        os.write(buffer, 0, buffer.length);
+        os.close();
+
+        System.out.println("JPEG acabat amb èxit.");
     }
 }
