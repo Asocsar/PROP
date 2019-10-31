2593a9e5b9df3e19305dd0164c9ecb63b6254e66
joanmaller <joanmaller@gmail.com>
2019-10-31 03:40:25 +0100

    Compress and decompress with buffs and file in out

--- src/Algoritmes/JPEG.java
+++ src/Algoritmes/JPEG.java
@@ -1,16 +1,11 @@
 package Algoritmes;
-import com.sun.org.apache.xml.internal.utils.SystemIDResolver;
 
-import java.awt.*;
+
 import java.io.*;
-import java.sql.SQLOutput;
-import java.util.Scanner;
-
-
-
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class JPEG {
-
 
 
 
@@ -23,10 +18,8 @@ public class JPEG {
         for (u = 0; u < 8; ++u) {
             for (v = 0; v < 8; ++v) {
 
-                if (u == 0) au = 1 / Math.sqrt(2);
-                else au = 1;
-                if (v == 0) av = 1 / Math.sqrt(2);
-                else av = 1;
+                au = (u==0) ?  1/Math.sqrt(2) : 1;
+                av = (v==0) ?  1/Math.sqrt(2) : 1;
 
                 sum = 0;
                 for (x = 0; x < 8; ++x) {
@@ -47,6 +40,7 @@ public class JPEG {
         }
         return dct;
     }
+
 
     public static int[] compress8 ( int[][] m, boolean chroma){
 
@@ -117,42 +111,48 @@ public class JPEG {
         //With RLE and Hufmann (RUNLENGTH, SIZE) (AMPLITUDE)
     }
 
+
+
     public static int[][] idct (int[][] m){
 
-        double[][] idct = new double[8][8];
+        int[][] idct = new int[8][8];
         double au, av, sum;
-        int u, v, x, y, val;
+        int u, v, x, y;
 
-        for (u = 0; u < 8; ++u) {
-            for (v = 0; v < 8; ++v) {
-
-                if (u == 0) au = 1 / Math.sqrt(2);
-                else au = 1;
-                if (v == 0) av = 1 / Math.sqrt(2);
-                else av = 1;
+        for (x = 0; x < 8; ++x) {
+            for (y = 0; y < 8; ++y) {
 
                 sum = 0;
-                for (x = 0; x < 8; ++x) {
-                    for (y = 0; y < 8; ++y) {
-                        sum += au * av * Math.cos(((2 * x + 1) * u * Math.PI) / 16) * Math.cos(((2 * y + 1) * v * Math.PI) / 16);
+
+                for (u = 0; u < 8; ++u) {
+                    for (v = 0; v < 8; ++v) {
+                        au = (u==0) ?  1/Math.sqrt(2) : 1;
+                        av = (v==0) ?  1/Math.sqrt(2) : 1;
+                        sum += m[u][v] * au * av * Math.cos(((2 * x + 1) * u * Math.PI) / 16) * Math.cos(((2 * y + 1) * v * Math.PI) / 16);
                     }
+
                 }
 
-                idct[u][v] =  0.25 * sum ;
+                idct[x][y] = (int) Math.round(0.25 * sum);
             }
         }
 
-        int[][] intdct = new int[8][8];
+        System.out.println();
         for (x = 0; x < 8; x++) {
             for (y = 0; y < 8; y++){
-                intdct[x][y] = Math.min(Math.max(dct[x][y] + 128, 0), 255);
+                System.out.printf("%d\t", idct[x][y]);
+                if(idct[x][y] > 127) idct[x][y] = 255;
+                else idct[x][y] += 128;
             }
+            System.out.println();
         }
-        return intdct;
+        System.out.println();
+
+        return idct;
     }
 
 
-    public static int[][] decompress8 (int[] buff){
+    public static int[][] decompress8 (int[] buff, boolean chroma){
 
         //Decoding ZigZag (RLE)
         int[][] ZigZag = {
@@ -179,6 +179,7 @@ public class JPEG {
         }
         System.out.println();
 
+        System.out.println();
         for(int i = 0; i < 8; ++i){
             for(int j = 0; j < 8; ++j){
                 System.out.printf("%d\t", B[i][j]);
@@ -205,8 +206,10 @@ public class JPEG {
                         {99,99,99,99,99,99,99,99},
                         {99,99,99,99,99,99,99,99}};
 
-        //if (chroma) Q = QC; //Si el boolean chroma es true aplicar el quantitzation de chrominance
+        if (chroma) Q = QC; //Si el boolean chroma es true aplicar el quantitzation de chrominance
         int[][] D = new int[8][8];
+
+        System.out.println();
         for(int i = 0; i < 8; ++i) {
             for (int j = 0; j < 8; ++j) {
                 D[i][j] = B[i][j] * Q[i][j];
@@ -217,41 +220,54 @@ public class JPEG {
 
 
         //DCT Transform
-        int[][] m = idct(D);
-
-        //Shift values by 128 and apply Math.min and Math.max
-        return m;
+        return idct(D);
     }
 
-    public static int[] compress(int[] buff, int  height, int width){
 
-        /*
-        int height = buff.length;
-        int width = m[0].length;
 
-        //Convert RGB to YCbCr color space
+    public static int[][] compress(int[][] Y, int[][] Cb, int[][] Cr){
 
-        int [][] Y = new int[height][width];
-        int [][] Cb = new int[height][width]; //height/factor_downsampling and width/FD
-        int [][] Cr = new int[height][width]; //H/fd and w/fd ??
+        int height = Y.length;
+        int width = Y[0].length;
+        int length = height*width /8;
+        int[][] buff = new int[length][];
 
-        int r,g,b;
-        while (i != )
-        for(int x = 0; x < height; ++x){
-            for(int y = 0; y < width; ++y){
-
-                r = buff[i];
-                g = buffer[i + 1];
-                b = buffer[i + 2];
-
-                Y[x][y] = (int) (0.299*r + 0.587*g + 0.114*b);
-                Cb[x][y] = (int) (128 - 0.1687*r - 0.3313*g + 0.5*b);
-                Cr[x][y] = (int) (128 + 0.5*r - 0.4187*g - 0.0813*b);
+        for(int i = 0; i < length; ++i){
+            int[][] m = new int[8][8];
+            for(int x = 0; x < 8; ++x){
+                for(int y =0; y < 8; ++y){
+                    m[x][y] = Y[i/8][i%8];
+                }
             }
+            buff[i] = compress8(m,false);
         }
-        return new int[][][] {Y,Cb,Cr};
 
-         */
+        return buff;
+    }
+
+
+    public static int[][][] decompress(int[] buff, int height, int width) {
+
+        int[][] buff = new int[length][];
+
+        for(int i = 0; i < length; ++i){
+            int[][] m = new int[8][8];
+            for(int x = 0; x < 8; ++x){
+                for(int y =0; y < 8; ++y){
+                    m[x][y] = Y[i/8][i%8];
+                }
+            }
+            buff[i] = decompress8(m,false);
+        }
+
+        return buff;
+
+    }
+
+
+    public static void main(String[] args) {
+        /*
+
         int[][] m = {
                 { 52, 55, 61, 66, 70, 61, 64, 73},
                 { 63, 59, 55, 90, 109, 85, 69, 72 },
@@ -263,7 +279,8 @@ public class JPEG {
                 { 87, 79, 69, 68, 65, 76, 78, 94 }};
 
 
-        int[][]m2 = decompress8(compress8(m, false));  //Returns array of ints of Y
+        int[][]m2 = decompress8(compress8(m, false), false);  //Returns array of ints of Y
+
 
         for(int i = 0; i < 8; ++i){
             for(int j = 0; j < 8; ++j){
@@ -271,77 +288,97 @@ public class JPEG {
             }
             System.out.println();
         }
+        */
 
+        try {
 
-
-        }
-
-    }
-
-
-
-    public static void decompress (double[][] m) { }
-
-    public static void main(String[] args) {
-
-        //try {
-
-            /*
             File infile = new File("/home/maller/Downloads/tiny.ppm");
             File outfile = new File("/home/maller/Downloads/tinessed.ppm");
 
-
+            /*
             long size = infile.length(); //Tamany del fitxer per posar-lo al buffer
             int[] buffer = new int[(int) size];
+            */
 
             FileInputStream fis = new FileInputStream(infile);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+            DataInputStream dis = new DataInputStream(new BufferedInputStream(fis));
 
-            System.out.println("Inici fitxer: ");
+            int width, height;
 
-            System.out.println((char)buffer[0] + " " + (char)buffer[1]); //Know if is P3 or P6
-            Then the second line is for knowing width and height
+            ArrayList<String> header = new ArrayList<>();
+            String line = br.readLine();
 
+            header.add(line); //ADD P6
+            while ((line = br.readLine()) != null && line.charAt(0) == '#') header.add(line);
 
-            for(int i = 0; i+2 < (int) size; i += 3){
-                System.out.println((char) buffer[i] + " " +  (char)buffer[i+1] + " " +(char) buffer[i+2]);
+            line = br.readLine(); //Read width and height
+            StringTokenizer st = new StringTokenizer(line);
+            width = Integer.parseInt(st.nextToken());
+            height = Integer.parseInt(st.nextToken());
+
+            int[][] Y = new int[height][width];
+            int[][] Cb = new int[height][width]; //height/factor_downsampling and width/FD
+            int[][] Cr = new int[height][width]; //H/fd and w/fd ??
+
+            br.readLine(); //Skip 255 -> depth
+
+            int r, g, b;
+            for (int i = 0; i < height; ++i) {
+                for (int j = 0; j < width; ++j) {
+                    r = dis.readInt();
+                    g = dis.readInt();
+                    b = dis.readInt();
+
+                    Y[i][j] = (int) (0.299 * r + 0.587 * g + 0.114 * b);
+                    Cb[i][j] = (int) (128 - 0.1687 * r - 0.3313 * g + 0.5 * b);
+                    Cr[i][j] = (int) (128 + 0.5 * r - 0.4187 * g - 0.0813 * b);
+
+                }
             }
-            //System.out.println(Arrays.toString(buffer));
 
-
-            //Comprimir a buffer
-            int[] compressedbuffer = compress(buffer, height, width);
-
-            System.out.println("Compressed result: ");
-            //System.out.println(Arrays.toString(compressedbuffer));
-
-            //Write to file .fg
-
-
-            //Descomprimir a buffer
-            byte[] decompressedbuffer = decompress(compressedbuffer);
-
-            System.out.println("Decompressed result: ");
-            System.out.println(Arrays.toString(decompressedbuffer));
+            decompress(compress(Y, Cb, Cr), height, width);
 
 
             FileOutputStream fos = new FileOutputStream(outfile);
-            fos.write(decompressedbuffer);
+
+            for (String s : header) fos.write((s + "\n").getBytes());
+            fos.write((width + " " + height + "\n").getBytes());
+            fos.write(("255\n").getBytes());
+
+            //YCbCr to RGB and write
+            int y, cb, cr;
+
+            for (int i = 0; i < height; ++i) {
+                for (int j = 0; j < width; ++j) {
+
+                    y = (int) (1.16438 * (Y[i][j] - 16));
+                    cb = Cb[i][j];
+                    cr = Cr[i][j];
+                    r = (int) (-222.921 + y + (408.583 * cr) / 256);
+                    g = (int) (135.576 + y - (100.291 * cb) / 256 - (208.120 * cr) / 256);
+                    b = (int) (-276.836 + y + (516.412 * cb) / 256);
+
+
+                    fos.write((r + " " + g + " " + b + " ").getBytes());
+                }
+                fos.write(("\n").getBytes());
+            }
 
             fis.close();
             fos.close();
-            */
+
             System.out.println("JPEG acabat amb èxit.");
 
-            /*
         }
 
         catch (FileNotFoundException e) {
             System.out.println("El fichero no ha sido encontrado");
 
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             System.out.println("Error en la entrada salida");
         }
-        */
 
     }
 }
