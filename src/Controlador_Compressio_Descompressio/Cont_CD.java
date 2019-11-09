f282502da2e01082bae8597b13f3ae2634a9253a
Daniel Cano Carrascosa <asocsar98@gmail.com>
2019-11-09 13:08:39 +0100

    Enlazando

--- src/Controlador_Compressio_Descompressio/Cont_CD.java
+++ src/Controlador_Compressio_Descompressio/Cont_CD.java
@@ -3,6 +3,7 @@ package Controlador_Compressio_Descompressio;
 import Algoritmes.LZ78;
 import Algoritmes.LZSS;
 import Algoritmes.LZW;
+import Controlador_ficheros.classe_fichero;
 import Estadístiques.Estadistiques;
 
 import java.io.*;
@@ -16,84 +17,78 @@ public class Cont_CD {
     }
 
 
-    public static void compressio_descompressio(String path, int id, String path_dest, boolean comprimir) throws IOException {
-        Item I = new Item();
-        I.getid();
+    public void compressio_descompressio(classe_fichero I) throws IOException {
         Object L = null;
         double time = 0;
         double rate = 0;
         Estadistiques E = new Estadistiques();
-        File file = new File(path);
-        BufferedReader br = new BufferedReader(new FileReader(file));
+        int id = I.getid();
+        boolean comprimir = I.compress();
         switch (id) {
             case 1:
                 LZ78 L8 = new LZ78();
                 if (comprimir) {
-                    L = L8.compresio(br);
-                    //time = L8.;
-                    //rate = L8.getRate();
-                    E.act8(0,0);
+                    L = L8.compresio(I.getBuffer());
+                    time = L8.getTime();
+                    rate = L8.getRate();
+                    E.act8(time,rate);
                 }
                 else {
-                    L = L8.descompresio();
-                    //time = L8.getTime();
-                    //rate = L8.getRate();
+                    L = L8.descompresio(I.getinfo());
                 }
 
                 break;
             case 2:
                 LZSS LS = new LZSS();
                 if (comprimir) {
-                    L = LS.compress_mine2(br);
+                    L = LS.compress_mine2(I.getBuffer());
                     time = LS.getTime();
                     rate = LS.getRate();
                     E.actS(time,rate);
                 }
                 else {
-                    L = LS.decompress(I.getcode());
-                    time = LS.getTime();
-                    rate = LS.getRate();
+                    L = LS.decompress(I.getinfo());
                 }
                 break;
             case 3:
                 LZW LW = new LZW();
                 if (comprimir) {
-                    L = LW.compress(br);
+                    L = LW.compress(I.getBuffer());
                     time = LW.getTime();
                     rate = LW.getRatio();
                     E.actW(time,rate);
 
                 }
                 else {
-                    L = LW.descomprimir(I.getcode());
-                    //time = LW.getTime();
-                    //rate = LW.getRate();
+                    L = LW.descomprimir(I.getinfo());
 
                 }
                 break;
-            /*case 4:
+            case 4:
                 JPEG JG = new JPEG();
                 if (comprimir) {
-                    L = JG.compress(br);
+                    L = JG.compress(I.getBuffer());
                     time = JG.getTime();
                     rate = JG.getRate();
                 }
                 else {
-                    S = JG.descomprimir(I.getcode());
+                    S = JG.descomprimir(I.getinfo());
                     time = JG.getTime();
                     rate = JG.getRate();
                 }
-                break;*/
+                break;
             }
 
         if (comprimir) {
-            path1 = path;
-            path2 = path_dest;
+            path1 = I.getpath();
+            path2 = I.getpat_dest();
         }
+
+        I.writeFile(L);
 
     }
 
-    public static void comparar() throws IOException {
+    public void comparar() throws IOException {
         System.out.println(path1);
         File file1 = new File(path1);
         BufferedReader br = new BufferedReader(new FileReader(file1));
@@ -106,6 +101,7 @@ public class Cont_CD {
         }
         System.out.println(S);
         System.out.println();
+
         File file2 = new File(path2);
         BufferedReader gr = new BufferedReader(new FileReader(file2));
         n = gr.read();
@@ -117,12 +113,12 @@ public class Cont_CD {
         System.out.println(S);
     }
 
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         try {
             compressio_descompressio("/home/asocar/Desktop/Ejemygplo.txt", 3, "/home/asocar/Desktop/Ejemplo_salida.LZW", true);
             comparar();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-    }
+    }*/
 }
