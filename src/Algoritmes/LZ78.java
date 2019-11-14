7723cd5b3e44a98d52812f94e901927703455359
paubl7 <56512228+paubl7@users.noreply.github.com>
2019-11-14 14:12:12 +0100

    Update LZ78.java

--- src/Algoritmes/LZ78.java
+++ src/Algoritmes/LZ78.java
@@ -1,131 +1,113 @@
 package Algoritmes;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.lang.String;
 
-public class LZ78 {
+public class LZ78_2 {
+    public  long elapsed_time;
+    public  long length_n;
+    public  long length_c;
 
-    public long elapsed_time;
-    public long length_n;
-    public long length_c;
-
-    public long get_time(){
+    public long get_time() {
 
         return elapsed_time;
     }
 
-
-    private String ajuntar_llistes(List<Integer> a, List<Character> b){
-        int n= a.size();
-        String aux="";
-        int aux_num;
-        for (int i= 0; i < n; ++i){
-            aux_num= a.get(i);
-            aux = aux + aux_num + "/" + b.get(i);
+    public byte[] transform(List<Byte>a){
+        byte[] n = new byte[a.size()];
+        for(int i=0; i < a.size(); ++i){
+            n[i]= a.get(i);
         }
-        return aux;
+        return n;
     }
 
-    public String compresio(BufferedReader file) throws IOException {
+    public byte[] compresio(byte[] fole) throws IOException {
         long startTime = System.currentTimeMillis();
-        length_n=0;
-        List<Integer> Indexs = new ArrayList<>();
-        List<Character> Coded_text = new ArrayList<>();
+        int x=0;
+        Byte[] file = new Byte[fole.length];
+        for(byte a : fole){
+            file[x++] = a;
+        }
+        length_n = fole.length;
         List<String> Caracters = new ArrayList<>();
         Caracters.add(null);
-        int lletra = file.read();
-        String aux_s = "";
-        int index = 0;
-        while (lletra != -1) {
-            aux_s = aux_s + (char) lletra;
-            if (!Caracters.contains(aux_s)) {
-                Caracters.add(aux_s);
-                Coded_text.add((char) lletra);
-                Indexs.add(index);
-                aux_s = "";
-                index = 0;
+        List<Byte> aux_byte = new ArrayList<>();
+        Byte lletra = file[0];
+        byte index = 0;
+        int i = 1;
+        String aux_s= "";
+        while (i < file.length) {
+            if (Caracters.size() < 127) {
+                aux_s = aux_s + (char)(lletra.byteValue());
+                if (!Caracters.contains(aux_s)) {
+                    Caracters.add(aux_s);
+                    aux_byte.add(index);
+                    aux_byte.add(lletra);
+                    index = 0;
+                    aux_s="";
+                } else {
+                    index = (byte)Caracters.indexOf(aux_s);
+                }
+                ++length_n;
+                lletra = file[i];
+                ++i;
+            } else {
+                Caracters = new ArrayList<>();
+                Caracters.add(null);
             }
-            else {
-                index=  Caracters.indexOf(aux_s);
-            }
-            ++length_n;
-            lletra = file.read();
         }
-        String Coded= ajuntar_llistes(Indexs,Coded_text);
         long endTime = System.currentTimeMillis();
-        elapsed_time= (endTime - startTime);
-        length_c= Coded.length();
-        return Coded;
+        elapsed_time = (endTime - startTime);
+        length_c = aux_byte.size();
+
+        return transform(aux_byte);
     }
 
-    public String escriure_llistes(Object b){
-        List<String> a= (List<String>)b;
-        int n= a.size();
-        String aux="";
-        for (int i= 1; i < n; ++i){
-            String r= a.get(i);
-            aux = aux + r.substring(4);
+    public String listtostring(List<String> a) {
+        int n = a.size();
+        String aux = "";
+        for (int i = 1; i < n; ++i) {
+            aux = aux + a.get(i);
 
         }
         return aux;
     }
 
-    public String descompresio(BufferedReader file) throws IOException {
+    public String descompresio(byte[] aux) throws IOException {
         long startTime = System.currentTimeMillis();
         List<String> Caracters = new ArrayList<>();
         Caracters.add(null);
-        int lletra = file.read();
-        char aux_lletra = 0;
-        int aux_in = 0;
         String aux_c = "";
-        boolean trobat = false;
-        while (lletra != -1) {
-            if ((char) lletra == '/' && aux_lletra == '/') {
-                aux_c = Caracters.get(aux_in) + (char) lletra;
-                Caracters.add(aux_c);
-                aux_lletra = 0;
-                aux_c = "";
-                lletra= file.read();
-                aux_in=0;
+        int index=0;
+        for(int i = 0; i < aux.length; ++i){
+            if (i%2 == 0 | i==0){
+                index= aux[i];
             }
-
-            else {
-                if (aux_lletra == '/') {
-                    aux_c = Caracters.get(aux_in) + (char) lletra;
+            else{
+                if(index== 0){
+                    aux_c= aux_c + (char) aux[i];
                     Caracters.add(aux_c);
-                    aux_lletra = 0;
                     aux_c="";
-                    lletra=file.read();
-                    aux_in=0;
                 }
-                else {
-                    trobat=false;
-                    while (lletra != -1 && !trobat){
-                        if ((char)lletra == '/') {
-                            trobat = true;
-                            aux_lletra = '/';
-                            lletra= file.read();
-                        }
-                        else {
-                            aux_in= aux_in*10 + Character.getNumericValue((char)lletra);
-                            lletra= file.read();
-                        }
-                    }
-
+                else{
+                    aux_c= aux_c + Caracters.get(index) + (char) aux[i];
+                    Caracters.add(aux_c);
+                    aux_c="";
                 }
             }
         }
-        String r= escriure_llistes(Caracters);
         long endTime = System.currentTimeMillis();
-        elapsed_time= (endTime - startTime);
-        return r;
-
+        elapsed_time = (endTime - startTime);
+        return listtostring(Caracters);
     }
 
-    public Long get_ratio_c(){
-        return length_n/length_c;
+    public Long get_ratio_c() {
+        return length_n / length_c;
     }
+    
+}
 
 
