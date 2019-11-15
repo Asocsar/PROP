de407322c2c45d97cfdda6e08bf66c8f5904614d
Daniel Cano Carrascosa <asocsar98@gmail.com>
2019-11-15 10:24:24 +0100

    Enlazando

---
+++ src/Controlador_Compressio_Descompressio/Algoritmes_Stub/LZ78.java
@@ -0,0 +1,31 @@
+package Controlador_Compressio_Descompressio.Algoritmes_Stub;
+
+import java.io.BufferedReader;
+import java.util.Arrays;
+import java.util.List;
+
+public class LZ78 {
+
+    public double rate = 2.0;
+    public double time = 100;
+
+    public LZ78 () {}
+
+    public double getTime() {
+        return time;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public List<Integer> compress (BufferedReader file) {
+        List<Integer> L = Arrays.asList(76,111,114,101);
+        return L;
+    }
+
+    public String descomprimir (List<Integer> s) {
+        String S = "Hola";
+        return S;
+    }
+}
---
+++ src/Controlador_Compressio_Descompressio/Algoritmes_Stub/LZW.java
@@ -0,0 +1,33 @@
+package Controlador_Compressio_Descompressio.Algoritmes_Stub;
+
+import java.io.BufferedReader;
+import java.util.Arrays;
+import java.util.List;
+
+public class LZW {
+
+    public double rate = 2.0;
+    public double time = 100;
+
+    public LZW () {
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public List<Integer> compress (BufferedReader file) {
+
+        List<Integer> L = Arrays.asList(76,111,114,101);
+        return L;
+    }
+
+    public String descomprimir (List<Integer> s) {
+        String S = "Hola";
+        return S;
+    }
+}
