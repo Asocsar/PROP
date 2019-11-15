3b9ab0ad9567677a3634b9437586ce2c26447733
paubl7 <56512228+paubl7@users.noreply.github.com>
2019-11-15 15:49:01 +0100

    Update LZ78.java

--- src/Algoritmes/LZ78.java
+++ src/Algoritmes/LZ78.java
@@ -1,22 +1,21 @@
 package Algoritmes;
 
 import java.io.*;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.lang.String;
 
-public class LZ78_2 {
-    public  long elapsed_time;
-    public  long length_n;
-    public  long length_c;
+public class LZ78 {
+    public static long elapsed_time;
+    public static long length_n;
+    public static long length_c;
 
-    public long get_time() {
+    /*public long get_time() {
 
         return elapsed_time;
-    }
+    }*/
 
-    public byte[] transform(List<Byte>a){
+    public static byte[] transform(List<Byte>a){
         byte[] n = new byte[a.size()];
         for(int i=0; i < a.size(); ++i){
             n[i]= a.get(i);
@@ -24,7 +23,7 @@ public class LZ78_2 {
         return n;
     }
 
-    public byte[] compresio(byte[] fole) throws IOException {
+    public static byte[] compresio(byte[] fole) throws IOException {
         long startTime = System.currentTimeMillis();
         int x=0;
         Byte[] file = new Byte[fole.length];
@@ -40,7 +39,8 @@ public class LZ78_2 {
         int i = 1;
         String aux_s= "";
         while (i < file.length) {
-            if (Caracters.size() < 127) {
+
+            if (Caracters.size()-1 < 127) {
                 aux_s = aux_s + (char)(lletra.byteValue());
                 if (!Caracters.contains(aux_s)) {
                     Caracters.add(aux_s);
@@ -54,8 +54,9 @@ public class LZ78_2 {
                 ++length_n;
                 lletra = file[i];
                 ++i;
+
             } else {
-                Caracters = new ArrayList<>();
+                Caracters.clear();
                 Caracters.add(null);
             }
         }
@@ -66,48 +67,61 @@ public class LZ78_2 {
         return transform(aux_byte);
     }
 
-    public String listtostring(List<String> a) {
+    public static String listtostring(List<String> a) {
         int n = a.size();
         String aux = "";
-        for (int i = 1; i < n; ++i) {
+        for (int i = 0; i < n; ++i) {
             aux = aux + a.get(i);
 
         }
         return aux;
     }
 
-    public String descompresio(byte[] aux) throws IOException {
+    public static String descompresio(byte[] aux) throws IOException {
         long startTime = System.currentTimeMillis();
         List<String> Caracters = new ArrayList<>();
+        List<String> Caracters_aux = new ArrayList<>();
         Caracters.add(null);
         String aux_c = "";
         int index=0;
         for(int i = 0; i < aux.length; ++i){
-            if (i%2 == 0 | i==0){
-                index= aux[i];
-            }
-            else{
-                if(index== 0){
-                    aux_c= aux_c + (char) aux[i];
-                    Caracters.add(aux_c);
-                    aux_c="";
+            if(Caracters.size() != 128) {
+                if (i % 2 == 0 | i == 0) index = aux[i];
+
+                else {
+                    if (index == 0) {
+                        aux_c = "";
+                        aux_c = aux_c + (char) aux[i];
+                        Caracters.add(aux_c);
+                        aux_c = "";
+                    }
+                    else {
+                        aux_c = aux_c + Caracters.get(index) + (char) aux[i];
+                        Caracters.add(aux_c);
+                        aux_c = "";
+                    }
                 }
-                else{
-                    aux_c= aux_c + Caracters.get(index) + (char) aux[i];
-                    Caracters.add(aux_c);
-                    aux_c="";
-                }
             }
+           else {
+                Caracters.remove(0);
+                Caracters_aux.addAll(Caracters);
+                Caracters.clear();
+                Caracters.add(null);
+                index= 0;
+
+             }
+
         }
+        Caracters.remove(0);
+        Caracters_aux.addAll(Caracters);
         long endTime = System.currentTimeMillis();
         elapsed_time = (endTime - startTime);
-        return listtostring(Caracters);
+        return listtostring(Caracters_aux);
     }
 
-    public Long get_ratio_c() {
+    public static Long get_ratio_c() {
         return length_n / length_c;
     }
-    
-}
 
+}
 
