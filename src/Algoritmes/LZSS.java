c51186af46d635c503bd791f9a39732fb1e11e7d
Clums98 <alex.sanchez.casals@gmail.com>
2019-11-17 09:24:15 +0100

    LZSS mask de 7 bits compila compress+decompress

--- src/Algoritmes/LZSS.java
+++ src/Algoritmes/LZSS.java
@@ -9,16 +9,8 @@ import java.lang.*;
 
 public class LZSS {
 
-    public LZSS() {
-    }
+    public LZSS() { }
 
-    ;
-
-    public static int flags = 0b00000000;
-
-    public static int flagindex = 1;
-
-    //public static int uconst = 128;
 
     public static double filesize;
 
@@ -28,32 +20,45 @@ public class LZSS {
 
     public static int search_buffer;
 
+    //DESCRIPCIÓ DEL MÈTODE : Obtenim el ratio de compressió
+    //PRE: Cert
+    //POST: Es retorna el ratio de compressió.
+    //EXCEPCIONS:
 
     public static double getRate() {
         return CompressRatio;
     }
 
+
+    //DESCRIPCIÓ DEL MÈTODE : Obtenim el temps de compressió
+    //PRE: Cert
+    //POST: Es retorna el temps de compressió.
+    //EXCEPCIONS:
+
     public static double getTime() {
         return CompressTime;
     }
 
+    //DESCRIPCIÓ DEL MÈTODE : Càlcul del ratio de compressió
+    //PRE: srcsize > 0
+    //POST: Es retorna el quocient entre la mida de l'arxiu comprimit i la mida de l'arxiu original, en bytes
+    //EXCEPCIONS: La mida de l'arxiu font és 0 -> quocient indeterminat
+
+
     public static double CalcCompressRatio(double srcsize, double compressedsize) {
+        try {
+            if (srcsize <= 0) throw new ArithmeticException();
+            return compressedsize / srcsize;
 
-        return compressedsize / srcsize;
-    }
-
-
-
-
-    private static int getMatchedLen(CharSequence src, int i1, int i2, int end) {
-        int n = Math.min(i2 - i1, end - i2);
-        for (int i = 0; i < n; i++) {
-            if (src.charAt(i1++) != src.charAt(i2++)) return i;
+        } catch (ArithmeticException a) {
+            System.out.print("Mida de l'arxiu font invàlida");
         }
-        return 0;
     }
 
-
+    //DESCRIPCIÓ DEL MÈTODE : Càlcul del ratio de compressió
+    //PRE: srcsize > 0
+    //POST: Es retorna el quocient entre la mida de l'arxiu comprimit i la mida de l'arxiu original, en bytes
+    //EXCEPCIONS: La mida de l'arxiu font és 0 -> quocient indeterminat
 
 
     public static Byte[] compress(byte[] file) throws IOException {
@@ -66,32 +71,33 @@ public class LZSS {
         boolean found = false;
         String mask = "0";
 
-        List<Byte> result = new ArrayList<>();
-        List<Byte> encoded = new ArrayList<>();
+        List<Byte> result = new ArrayList<>(); //Llista temporal
+        List<Byte> encoded = new ArrayList<>(); // Llista definitiva
         result.add(file[0]);
 
 
         if (filesize < 4095) search_buffer = 0;
         else search_buffer = 4095;
-        while (SB < LAB & LAB < file.length) {
-
-            if (mask.length() == 7) {
+        //Recorrem l'arxiu
+        while ( LAB < file.length) {
+            //Si la mascara te mida 7, codifiquem en la llista definitiva mask+llista temporal
+            if (mask.length() == 7 ) {
                 encoded.addAll((encode(mask, result)));
                 mask = "";
                 result.clear();
             }
-
-            if (found && (match == 127 || ((SB == LABini - 1 || file[LAB] != file[SB]) && match > 2))) {
+            //Generem una parella <match,offset> si : hem trobat el match màxim, o els elements actuals ja no coincideixen i el match > 3
+            if (found && (match == 127 || ((SB == LABini || file[LAB] != file[SB]) && match > 3))) {
 
                 byte binmatch = (byte) (match);
-                byte binoffsetl = ((byte) (offset));
+                byte binoffsetl = ((byte) (offset));  // 3<offset<4096 --> s'ha de codificar en 2 bytes
                 byte binoffseth = (byte) (offset >> 8);
 
                 result.add(binmatch);
                 result.add(binoffseth);
                 result.add(binoffsetl);
 
-                mask += '1';
+                mask += '1'; // Com hem generat una parella, l'element següent de la màscara és 1
 
                 match = 0;
                 offset = 0;
@@ -101,26 +107,28 @@ public class LZSS {
                 else SB = 0;
 
             }
+            //Si anem trobant elements coincidents augmentem el match
             else if (file[LAB] == file[SB] && SB < LABini) {
                 if (!found) {
-                    offset = LAB - SB;
+                    offset = LAB - SB; // Si és el primer element coincident (trobat == false), guardem la diferència entre ambdós (offset)
                     found = true;
                 }
                 match++;
                 SB++;
                 LAB++;
             }
-            else if ((found && match < 3 && (file[LAB] != file[SB] || SB == LABini))  || (!found && SB == LAB - 1)) {
+            //Si no hem trobat cap element coincident, o bé hem trobat el primer que no ho és i el match actual és < 4, codifiquem com a caràcter
+            else if ((found && match < 4 && (file[LAB] != file[SB] || SB == LABini ))  || (!found && SB == LAB - 1)) {
                 byte notmatched;
                 if (found) {
-                    notmatched = file[LAB - match];
+                    notmatched = file[LAB - match]; //Si hi havia coincidència hem de restar match de l'element que ens trobem per situar-nos en l'actual
                     found = false;
                     LAB -= match;
-                } else notmatched = file[LAB];
+                } else notmatched = file[LAB]; //Sino, l'actual és el que toca codificar
 
                 result.add(notmatched);
 
-                mask += '0';
+                mask += '0'; //Hem afegit un caràcter, per tant el següent bit de la mascara és 0.
 
                 LAB++;
                 LABini = LAB;
@@ -132,13 +140,13 @@ public class LZSS {
             }
             else ++SB;
         }
-        Byte[] byteencoding = encoded.toArray(new Byte[encoded.size()]);
+        Byte[] byteencoding = encoded.toArray(new Byte[encoded.size()]); //Convertim la llista a un byte[}
         return byteencoding;
     }
 
 
     public static List<Byte> encode(String mask, List<Byte> current) {
-
+        //Codifiquem en una sola llista la màscara actual i els elements corresponents a aquesta màscara
         List<Byte> encoded = new ArrayList<>();
         Byte Bmask = Byte.parseByte(mask,2);
         encoded.add(Bmask);
@@ -155,27 +163,33 @@ public class LZSS {
 
 
 
-    //DESCRIPCIÓ DEL MÈTODE
+    //DESCRIPCIÓ DEL MÈTODE:
     //PRE:
     //POST:
-    //EXCEPCIONES
+    //EXCEPCIONES:
 
-    public static StringBuilder decompress (Byte[] Bencoded) {
+    public static StringBuilder decompress (Byte[] Bencoded)  {
         List<Byte> encoded = Arrays.asList(Bencoded);
         StringBuilder result = new StringBuilder();
         int resultindex = 0;
+        //Recorrem els bytes codificats
         for (int i = 0; i < encoded.size();) {
+
+            //Obtenim la màscara i la posem en 7 bits
+
             String binmask = Integer.toBinaryString(((int) encoded.get(i++)) & 0xFF);
             while (binmask.length() < 7) binmask = "0" + binmask;
-            for (int maskind = 0; maskind < binmask.length(); ++maskind) {
 
+            //Recorrem la màscara de bits per identificar què són caràcters (0) i què són parelles <match,offset> (1)
+
+            for (int maskind = 0; maskind < binmask.length(); ++maskind) {
+                //Obtenim caràcters
                 if (binmask.charAt(maskind) == '0') {
                     result.append((char) (encoded.get(i++)).intValue());
                     resultindex++;
                 }
-
+                //Decsodifiquem parelles
                 else {
-
                     String offset,offsetl,offseth;
 
                     offsetl = Integer.toBinaryString(encoded.get(i+2).intValue()&0xFF);
@@ -217,9 +231,8 @@ public class LZSS {
     public static void WriteatFile (StringBuilder decompressed) throws IOException{
         File out = new File("/home/clums/Escriptori/Uncompressed.fS");
         FileWriter write = new FileWriter(out);
-        PrintWriter pw = new PrintWriter(write);
         for (int i = 0; i < decompressed.length(); ++i) {
-            pw.print(decompressed.charAt(i));
+            write.write(decompressed.charAt(i));
         }
     }
 
@@ -230,9 +243,9 @@ public class LZSS {
 
         double startTime = System.currentTimeMillis();
 
-        File file = new File("/home/clums/Escriptori/JOCS_DE_PROVA/Non_Extended_ASCII/UnMundoFeliz-Non_extended.txt");
+        File file = new File("/home/clums/Escriptori/Ejemplo.txt");
         byte[] bytefile = Files.readAllBytes(file.toPath());
-        filesize = bytefile.length*8;
+        filesize = bytefile.length;
 
 
         Byte[] compressed = compress(bytefile);
@@ -242,10 +255,8 @@ public class LZSS {
         double endTime = System.currentTimeMillis();
         CompressTime = (endTime - startTime);
 
-        // Byte[] voidl = {(byte) 10, (byte) 112,(byte) 97,(byte) 97,(byte) 112,(byte) 2,(byte) 0,(byte) 4,(byte) 112,(byte) 3,(byte) 0,(byte) 4,(byte) 98};
-        //Object o = voidl;
         StringBuilder decompressed = decompress(compressed);
-        CompressRatio = CalcCompressRatio(filesize, compressed.length*8);
+        CompressRatio = CalcCompressRatio(filesize, compressed.length);
         //WriteatFile(decompressed);
         print_status(compressed,decompressed);
     }
