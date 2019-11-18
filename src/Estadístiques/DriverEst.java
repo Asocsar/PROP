681ef1f3d754e93e2fabc624d6eeea23e67b9aeb
asocar <danielllisto@gmail.com>
2019-11-18 11:07:50 +0100

    Actual

--- src/Estadístiques/DriverEst.java
+++ src/Estadístiques/DriverEst.java
@@ -1,7 +1,8 @@
 package Estadístiques;
 import org.junit.Test;
-import org.junit.Assert;
 import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class DriverEst {
@@ -20,11 +21,11 @@ public class DriverEst {
         n4 = S.nextDouble();
         n5 = S.nextInt();
         Estadistiques.setLZW(n1,n2,n3,n4,n5);
-        Assert.assertEquals("Correct!",n1,Estadistiques.getTimeLZW());
-        Assert.assertEquals("Correct!",n2,Estadistiques.getGlobTimeLZW());
-        Assert.assertEquals("Correct!",n3,Estadistiques.getRatioLZW());
-        Assert.assertEquals("Correct!",n4,Estadistiques.getGlobRatioLZW());
-        Assert.assertEquals("Correct!",n5,Estadistiques.getQuantLZW());
+        assertEquals("Correct!",n1,Estadistiques.getTimeLZW(), 0);
+        assertEquals("Correct!",n2/n5,Estadistiques.getGlobTimeLZW(), 0);
+        assertEquals("Correct!",n3,Estadistiques.getRatioLZW(), 0);
+        assertEquals("Correct!",n4/n5,Estadistiques.getGlobRatioLZW(), 0);
+        assertEquals("Correct!",n5,Estadistiques.getQuantLZW(), 0);
         System.out.println("Introduzca los siguientes parametros para LZSS");
         System.out.println("tiempo local - tiempo global - ratio local - ratio global - numero de veces de ejecución");
         n1 = S.nextDouble();
@@ -33,11 +34,11 @@ public class DriverEst {
         n4 = S.nextDouble();
         n5 = S.nextInt();
         Estadistiques.setLZSS(n1,n2,n3,n4,n5);
-        Assert.assertEquals("Correct!",n1,Estadistiques.getTimeLZW());
-        Assert.assertEquals("Correct!",n2,Estadistiques.getGlobTimeLZW());
-        Assert.assertEquals("Correct!",n3,Estadistiques.getRatioLZW());
-        Assert.assertEquals("Correct!",n4,Estadistiques.getGlobRatioLZW());
-        Assert.assertEquals("Correct!",n5,Estadistiques.getQuantLZW());
+        assertEquals("Correct!",n1,Estadistiques.getTimeLZW(), 0);
+        assertEquals("Correct!",n2/n5,Estadistiques.getGlobTimeLZW(), 0);
+        assertEquals("Correct!",n3,Estadistiques.getRatioLZW(), 0);
+        assertEquals("Correct!",n4/n5,Estadistiques.getGlobRatioLZW(), 0);
+        assertEquals("Correct!",n5,Estadistiques.getQuantLZW(), 0);
         System.out.println("Introduzca los siguientes parametros para LZ78");
         System.out.println("tiempo local - tiempo global - ratio local - ratio global - numero de veces de ejecución");
         n1 = S.nextDouble();
@@ -46,11 +47,11 @@ public class DriverEst {
         n4 = S.nextDouble();
         n5 = S.nextInt();
         Estadistiques.setLZ78(n1,n2,n3,n4,n5);
-        Assert.assertEquals("Correct!",n1,Estadistiques.getTimeLZW());
-        Assert.assertEquals("Correct!",n2,Estadistiques.getGlobTimeLZW());
-        Assert.assertEquals("Correct!",n3,Estadistiques.getRatioLZW());
-        Assert.assertEquals("Correct!",n4,Estadistiques.getGlobRatioLZW());
-        Assert.assertEquals("Correct!",n5,Estadistiques.getQuantLZW());
+        assertEquals("Correct!",n1,Estadistiques.getTimeLZW(), 0);
+        assertEquals("Correct!",n2/n5,Estadistiques.getGlobTimeLZW(), 0);
+        assertEquals("Correct!",n3,Estadistiques.getRatioLZW(), 0);
+        assertEquals("Correct!",n4/n5,Estadistiques.getGlobRatioLZW(), 0);
+        assertEquals("Correct!",n5,Estadistiques.getQuantLZW(), 0);
         System.out.println("Introduzca los siguientes parametros para JPEG");
         System.out.println("tiempo local - tiempo global - ratio local - ratio global - numero de veces de ejecución");
         n1 = S.nextDouble();
@@ -59,15 +60,15 @@ public class DriverEst {
         n4 = S.nextDouble();
         n5 = S.nextInt();
         Estadistiques.setJPEG(n1,n2,n3,n4,n5);
-        Assert.assertEquals("Correct!",n1,Estadistiques.getTimeLZW());
-        Assert.assertEquals("Correct!",n2,Estadistiques.getGlobTimeLZW());
-        Assert.assertEquals("Correct!",n3,Estadistiques.getRatioLZW());
-        Assert.assertEquals("Correct!",n4,Estadistiques.getGlobRatioLZW());
-        Assert.assertEquals("Correct!",n5,Estadistiques.getQuantLZW());
+        assertEquals("Correct!",n1,Estadistiques.getTimeLZW(), 0);
+        assertEquals("Correct!",n2,Estadistiques.getGlobTimeLZW(), 0);
+        assertEquals("Correct!",n3,Estadistiques.getRatioLZW(), 0);
+        assertEquals("Correct!",n4,Estadistiques.getGlobRatioLZW(), 0);
+        assertEquals("Correct!",n5,Estadistiques.getQuantLZW(), 0);
         System.out.println("Introduzca ultimo algoritmo utilizado");
         String s = S.next();
         Estadistiques.setLastAlg(s);
-        Assert.assertEquals("Correct!", s, Estadistiques.getLastAlg());
+        assertEquals("Correct!", s, Estadistiques.getLastAlg());
         Estadistiques.visualglob();
     }
 }
