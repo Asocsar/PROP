5fdd6baf0fa898858310dc52c6c6165b477260a8
Daniel Cano Carrascosa <asocsar98@gmail.com>
2019-12-07 17:09:15 +0100

    Interfaz

--- src/Controlador_Compressio_Descompressio/Cont_CD.java
+++ src/Controlador_Compressio_Descompressio/Cont_CD.java
@@ -118,8 +118,8 @@ public class Cont_CD {
     public void compressio_carpeta (String path_o, String path_d, String algoritme) throws IOException {
         id = algoritme;
         controlador_gestor_fitxer I = new controlador_gestor_fitxer();
-       // I.folder(path_o);
-        while (/*I.resten()*/true) {
+        I.folder(path_o);
+        while (I.resten()) {
             Object L = action(path_o, id, true, I);
             I.writeFile(L, path_o, path_d);
         }
@@ -129,17 +129,17 @@ public class Cont_CD {
     // Pre: Cert
     // Post: Descomprimeix el fitxer situat al path_o i el desa al path_d
     public void descompressio_fitxer (String path_o, String path_d, String algoritme) throws IOException {
-        id = algoritme;
         controlador_gestor_fitxer I = new controlador_gestor_fitxer();
+        id = I.getAlgoritme(path_o);
         Object L = action(path_o, id, false, I);
         I.writeFile(L, path_o, path_d);
     }
 
     // Pre: Cert
     // Post: Descomprimeix el fitxer situat al path_o i el desa al path_d
-    public void descompressio_carpeta (String path_o, String path_d, String algoritme) throws IOException {
-        id = algoritme;
+    public void descompressio_carpeta (String path_o, String path_d) throws IOException {
         controlador_gestor_fitxer I = new controlador_gestor_fitxer();
+        id = I.getAlgoritme(path_o);
         Object L = action(path_o, id, false, I);
         I.writeFile(L, path_o, path_d);
     }
@@ -152,7 +152,7 @@ public class Cont_CD {
     public String [] comparar() throws IOException {
         String [] K = new String[2];
         if (path1.equals("")) {
-
+            // AQUI TENGO QUE PONER UNA EXCEPCION QUE UN NO HE HECHO
         }
         else {
             controlador_gestor_fitxer I = new controlador_gestor_fitxer();
@@ -161,7 +161,7 @@ public class Cont_CD {
             K[0] = S;
             //descomprimir el contingut del fitxer comprimit i el mostrem
             Object L = action(path2, id, false, I);
-            //K[1] = I.compare(L);
+            K[1] = I.compare(L);
         }
         return  K;
     }
