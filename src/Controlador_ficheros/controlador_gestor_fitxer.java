5392a690056cbb13e2d314030e767d6670f63951
paubl7 <56512228+paubl7@users.noreply.github.com>
2019-12-08 19:21:43 +0100

    Update controlador_gestor_fitxer.java

--- src/Controlador_ficheros/controlador_gestor_fitxer.java
+++ src/Controlador_ficheros/controlador_gestor_fitxer.java
@@ -1,4 +1,7 @@
-package Controlador_ficheros;
+package Controlador_Gestor_Fitxer;
+
+
+import Gestor_fitxeros.gestor_fitxers;
 
 import java.io.IOException;
 
@@ -7,7 +10,9 @@ import java.io.IOException;
 public class controlador_gestor_fitxer {
 
     private Boolean C_P;
-    private Integer id_a;
+    private String id_a;
+    private gestor_fitxers gestor;
+    private boolean restante;
 
     //EXCEPCIONS
 
@@ -33,12 +38,19 @@ public class controlador_gestor_fitxer {
 
     }
 
+    public void folder(String Path_o){
+
+    }
+
+    public boolean resten(){
+        return restante;
+    }
+
     //PRE: Path_original ha de ser vàlid
     //POST: Retorna l’estructura de dades necessària obtinguda del fitxer del path_original, depenent de si és una compressió o descompressió i de l’algorisme que s’utilitzarà.
-    public Object get_buffer(String Path_original, Boolean c_p,Integer id_algorisme) throws IOException, FicheroCompressionNoValido, FicheroDescompressionNoValido {
-        gestor_fitxers gestor = new gestor_fitxers();
+    public Object get_buffer(String Path_original, Boolean c_p,String id) throws IOException, FicheroCompressionNoValido, FicheroDescompressionNoValido {
         C_P= c_p;
-        id_a= id_algorisme;
+        id_a= id;
         if (C_P) {
             String aux = Path_original.substring(Path_original.length() - 3);
             if (!aux.equals("txt") & !aux.equals("ppm")) {
@@ -46,18 +58,17 @@ public class controlador_gestor_fitxer {
             }
 
             else {
-                if ((aux.equals("txt") & id_algorisme == 4) | (aux.equals("ppm") & id_algorisme != 4)) {
+                if ((aux.equals("txt") & id_a.equals("JPEG")) | (aux.equals("ppm") & !id_a.equals("JPEG") )) {
                     throw new FicheroCompressionNoValido("El fichero seleccionado no es comaptible con el algorismo");
-                } else gestor.get_f_compressio(Path_original, id_algorisme);
+                } else gestor.get_f_compressio(Path_original);
             }
         }
 
         else {
             String aux = Path_original.substring(Path_original.length() - 2);
-            if (((aux.equals("f8")|aux.equals("fS") | aux.equals("fW"))& id_algorisme == 4) | (aux.equals("fJ") & id_algorisme != 4)) {
+            if((!aux.equals("f8")& !aux.equals("fS") & !aux.equals("fW") & !aux.equals("fG"))){
                 throw new FicheroDescompressionNoValido("El fichero no se puede descomprimir.");
             }
-
             else gestor.conversio_fitxer_desc(Path_original);
 
         }
@@ -67,21 +78,36 @@ public class controlador_gestor_fitxer {
     //PRE: path_og ha de ser vàlid
     //POST: Crea un fitxer amb el resultat de la compressió o descompressió passat per paràmetre.
     public void writeFile (Object write, String Path_desti,String path_og) throws IOException{
-        gestor_fitxers gestor = new gestor_fitxers();
         if(!C_P){
             gestor.c_e_fichero_descomp(path_og,Path_desti,write);
         }
 
-        else gestor.c_e_fichero_comp(Path_desti,write,id_a);
+        else gestor.c_e_fichero_comp(Path_desti,write,path_og); //ARREGLAR c_e_fichero_comp
     }
 
     //PRE: Cert
     //POST: Retorna el contingut del fitxer en el path "path" en un string
     public String obtenir_fitxer(String path) throws IOException {
-        gestor_fitxers gestor= new gestor_fitxers();
         String a=  gestor.read_file(path);
         return a;
 
     }
 
+    public String getAlgoritme (String path_o){
+        String aux= path_o.substring(path_o.length()-2);
+        switch(aux) {
+            case"f8": return "LZ78";
+
+            case"fS": return "LZSS";
+
+            case"fW": return "LZW";
+
+            case"fG": return "JPEG";
+
+            default:
+        }
+        return null;
+    }
+
 }
+
