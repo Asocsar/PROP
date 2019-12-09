8285d1c3b49c75d89f19d41c9b55c446fb6af019
paubl7 <56512228+paubl7@users.noreply.github.com>
2019-12-09 12:00:52 +0100

    Update gestor_fitxers.java

--- src/Controlador_ficheros/gestor_fitxers.java
+++ src/Controlador_ficheros/gestor_fitxers.java
@@ -1,14 +1,7 @@
 package Gestor_fitxeros;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import java.awt.image.WritableRaster;
 import java.io.*;
 import java.nio.file.*;
-import java.util.List;
-import java.io.File;
-import java.io.IOException;
 import java.lang.String;
 
 
@@ -17,11 +10,9 @@ import java.lang.String;
 
 public class gestor_fitxers {
     //VARIABLES DE LA CLASSE
-    private  String path_dest;
-    private Integer id;
-    private Integer id_desc;
-    private  String extensio;
-    private boolean c_p; //true=compressio false=descompresio
+    private static String nom_fitxer;
+    private static Integer id_desc;
+    private static String extensio;
 
 
     //PRE: Cert
@@ -30,63 +21,26 @@ public class gestor_fitxers {
 
     }
 
-    public void folder(){
+    public static void folder(){
 
     }
 
-    //COMPRESSIO: BUSCAR, LLEGIR ARXIU. (PRIVADES)
+    //COMPRESSIO:
 
-
-    //PRE:El path “path_og” és vàlid
-    //POST: Retorna un objecte que representa la estructura de dades necessària depenent de l’algorisme utilitzat i de si es fa una compressió o descompressió.
-    private byte[] buscar_leer_archivo(String path_og) throws IOException {
-            if(id== 4) {
-                File imgPath = new File(path_og);
-                BufferedImage bufferedImage = ImageIO.read(imgPath);
-                WritableRaster raster = bufferedImage .getRaster();
-                DataBufferByte data   = (DataBufferByte) raster.getDataBuffer();
-                return ( data.getData() );
-            }
-            else{
-                File file = new File (path_og);
-                byte[] aux1 = Files.readAllBytes(file.toPath());
-                return aux1;
-
-            }
+    //PRE: Id ha de ser un id d’algorisme vàlid. El path_og ha de ser vàlid.
+    //POST: Retorna un objecte amb l’estructura de dades necessària per la compressió de l’arxiu demanat per l’algorisme seleccionat.
+    public static byte[] get_f_compressio(String path_og, String id_a) throws IOException {
+        nombre_fichero(path_og);
+        ex_comp(id_a);
+        return buscar_leer_archivo(path_og);
     }
-
-
-    //FUNCIONS DE CREACIO I ESCRIPTURA (PUBLIQUES)
-
-    //PRE: path_og és un path vàlid.
-    //POST: Crea un fitxer en el path_desti i hi escriu el resultat de la descompressió.
-    public void c_e_fichero_descomp (String path_og, String path_desti, Object encoded) throws IOException {
-            Path path = Paths.get(path_desti);
-            id_algorismo(path_og);
-            path_dest = path + extensio;
-            if(id_desc==4){
-                    System.out.print("Escriure imatge descomprimida.");
-            }
-            else {
-                File file = new File(path_dest);
-                if(file.createNewFile()) {
-                    FileOutputStream fop = new FileOutputStream(file);
-                    fop.write((byte[])encoded);
-                    fop.flush();
-                    fop.close();
-                }
-            }
-
-    }
-
 
     //PRE: Id ha de ser un id d’algorisme vàlid
     //POST: Crea un fitxer i hi escriu el resultat de la compressió.
-    public void c_e_fichero_comp (String path_desti, Object aux, String id) throws IOException {
-        Path path = Paths.get(path_desti);
-        extension(id);
-        path_dest = path + extensio;
-        File file = new File(path_dest);
+    public static void c_e_fichero_comp (String path_desti, Object aux) throws IOException {
+        Path path_dest= Paths.get(path_desti,nom_fitxer);
+        String nom_ex= path_dest + extensio;
+        File file = new File(nom_ex);
         if (file.createNewFile()) {
             FileOutputStream fop= new FileOutputStream(file);
             fop.write((byte[])aux);
@@ -95,45 +49,66 @@ public class gestor_fitxers {
         }
     }
 
-    //FUNCIONS DE TRANSFORMACIO DE FITXERS (PUBLIQUES)
+    //PRE: Id ha de ser un id d’algorisme vàlid
+    //POST: La variable global extensió queda actualitzada.
+    private static void ex_comp(String id_s){
+        switch(id_s) {
+            case"LZ78":
+                extensio=".f8";
+                break;
+            case"LZSS":
+                extensio=".fS";
+                break;
+            case"LZW":
+                extensio=".fW";
+                break;
 
-    //PRE: Id ha de ser un id d’algorisme vàlid. El path_og ha de ser vàlid.
-    //POST: Retorna un objecte amb l’estructura de dades necessària per la compressió de l’arxiu demanat per l’algorisme seleccionat.
-    public byte[] get_f_compressio(String path_og) throws IOException {
-        c_p = true;
-        //ARREGLAR EXTENSIO
-        extension(path_og);
-        return buscar_leer_archivo(path_og);
+            case"JPEG":
+                extensio=".fG";
+                break;
+
+            default:
+        }
     }
+
+
+    //FI FUNCIONS COMPRESIO
+
+
+    //DESCOMPRESIO:
 
     //PRE: El path_og ha de ser vàlid.
     //POST: Retorna un objecte amb l’estructura de dades necessària per la descompressió de l’arxiu demanat.
-    public Object conversio_fitxer_desc(String path_og) throws IOException {
-        id_algorismo(path_og);
-        c_p = false;
-        id=0;
+    public static byte[] conversio_fitxer_desc(String path_og) throws IOException {
+        id_ex_desc(path_og);
+        nombre_fichero(path_og);
         return buscar_leer_archivo(path_og);
     }
 
-    //PRE: Cert
-    //POST: Retorna el contingut del fitxer en el path "path" en un string
-    public String read_file(String path) throws IOException {
-            Path path1 = Paths.get(path);
-            File file = new File(path);
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String aux = "";
-            String aux2;
-            while ((aux2 = br.readLine()) != null) {
-                aux = aux + aux2 + "/n";
+    //PRE: path_og és un path vàlid.
+    //POST: Crea un fitxer en el path_desti i hi escriu el resultat de la descompressió.
+    public static void c_e_fichero_descomp (String path_og, String path_desti, Object encoded) throws IOException {
+        String nom_ex= nom_fitxer + extensio;
+        Path path_dest= Paths.get(path_desti,nom_ex);
+        System.out.println(path_dest);
+        if(id_desc==4){
+            System.out.print("Escriure imatge descomprimida.");
+        }
+        else {
+            File file = new File(String.valueOf(path_dest));
+            if(file.createNewFile()) {
+                FileOutputStream fop = new FileOutputStream(file);
+                fop.write((byte[])encoded);
+                fop.flush();
+                fop.close();
             }
-            return aux;
-    }
+        }
 
-    //FUNCIONS AUXILIARS (PRIVADES)
+    }
 
     //PRE: path_og és un path vàlid
     //POST: Actualitza les variables id_desc i extensió depenent del path_og
-    private void id_algorismo(String path_og){
+    private static void id_ex_desc(String path_og){
         String aux= path_og.substring(path_og.length()-2);
         if(aux.equals("f8")) {
             id_desc = 1;
@@ -154,25 +129,35 @@ public class gestor_fitxers {
 
     }
 
-    //PRE: Id ha de ser un id d’algorisme vàlid
-    //POST: La variable global extensió queda actualitzada.
-    private void extension(String id_s){
-        if(id_s.equals("LZ78")) {
-            extensio= ".f8";
-            id= 1;
-        }
-        else if(id.equals("LZSS")) {
-            extensio= ".fS";
-            id=2;
-        }
-        else if(id.equals("LZW")) {
-            extensio= ".fW";
-            id=3;
-        }
-        else if(id.equals("JEPG")){
-            extensio= ".fG";
-            id=4;
-        }
+    //FI FUNCIONS DESCOMPRESIO
+
+    //PRE:El path “path_og” és vàlid
+    //POST: Retorna un objecte que representa la estructura de dades necessària depenent de l’algorisme utilitzat i de si es fa una compressió o descompressió.
+    private static byte[] buscar_leer_archivo(String path_og) throws IOException {
+        File file = new File (path_og);
+        byte[] aux1 = Files.readAllBytes(file.toPath());
+        return aux1;
+    }
+
+    //AGAFA EL NOM DEL FITXER ORIGINAL
+    private static void nombre_fichero(String Path_og){
+        Path p = Paths.get(Path_og);
+        String aux = p.getFileName().toString();
+        int pos = aux.lastIndexOf(".");
+        nom_fitxer=aux.substring(0, pos);
+    }
+
+    //PRE: Cert
+    //POST: Retorna el contingut del fitxer en el path "path" en un string
+    public static String read_file(String path) throws IOException {
+            File file = new File(path);
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String aux = "";
+            String aux2;
+            while ((aux2 = br.readLine()) != null) {
+                aux = aux + aux2 + "/n";
+            }
+            return aux;
     }
 
 }
