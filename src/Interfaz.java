f0909235b1ecd1bd38d972352cd0ba1283bc2904
Asocsar <44205390+Asocsar@users.noreply.github.com>
2019-12-13 18:44:31 +0100

    Add files via upload

--- src/Interfaz.java
+++ src/Interfaz.java
@@ -1,14 +1,20 @@
 
 import Controlador_Compressio_Descompressio.Cont_CD;
+import Controlador_ficheros.controlador_gestor_fitxer;
 
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.filechooser.FileFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Interfaz extends JFrame {
@@ -26,6 +32,7 @@ public class Interfaz extends JFrame {
         private int mode;
         public static final int MODE_OPEN = 1;
         public static final int MODE_SAVE = 2;
+        private boolean directori = false;
 
         public JFilePicker(String textFieldLabel, String buttonLabel) {
             this.textFieldLabel = textFieldLabel;
@@ -33,8 +40,6 @@ public class Interfaz extends JFrame {
 
             fileChooser = new JFileChooser();
             fileChooser.setAcceptAllFileFilterUsed(false);
-            //fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-            //fileChooser.removeChoosableFileFilter();
 
             setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 
@@ -58,19 +63,6 @@ public class Interfaz extends JFrame {
 
         }
 
-        public void selectmode (int n) {
-            if (n == 1) {
-                fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            }
-            else {
-                if (metodo == 1 && state == 0) {
-                    fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-                } else {
-                    fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-                }
-            }
-        }
-
         private void buttonActionPerformed(ActionEvent evt) {
             if (mode == MODE_OPEN) {
                 if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
@@ -84,9 +76,10 @@ public class Interfaz extends JFrame {
             }
         }
 
-        public void addFileTypeFilter(String extension, String description) {
-            FileTypeFilter filter = new FileTypeFilter(extension, description);
+        public void addFileTypeFilter(String[] e, String description) {
+            FileTypeFilter filter = new FileTypeFilter(e, description);
             fileChooser.addChoosableFileFilter(filter);
+
         }
 
         public void setMode(int mode) {
@@ -96,6 +89,8 @@ public class Interfaz extends JFrame {
         public String getSelectedFilePath() {
             return textField.getText();
         }
+
+        public JTextField gettextf () {return textField;}
 
         public JFileChooser getFileChooser() {
             return this.fileChooser;
@@ -109,14 +104,16 @@ public class Interfaz extends JFrame {
             fileChooser.removeChoosableFileFilter(fileChooser.getFileFilter());
             textField.setText("");
         }
+
+        public boolean getdir () {return directori;}
     }
 
     public static class FileTypeFilter extends FileFilter {
 
-        private String extension;
+        private String[] extension;
         private String description;
 
-        public FileTypeFilter(String extension, String description) {
+        public FileTypeFilter(String[] extension, String description) {
             this.extension = extension;
             this.description = description;
         }
@@ -126,111 +123,99 @@ public class Interfaz extends JFrame {
             if (file.isDirectory()) {
                 return true;
             }
-            return file.getName().toLowerCase().endsWith(extension);
+            for (String s : extension) {
+                if (file.getName().toLowerCase().endsWith(s)) return true;
+            }
+            return false;
         }
 
         public String getDescription() {
-            return description + String.format(" (*%s)", extension);
+            return description + Arrays.toString(extension);
+        }
+    }
+
+    public static class Boton extends JButton {
+        public Boton() {
+            this.setText("Accion");
         }
     }
 
     private JPanel Panel;
-    private JTextPane textPane2;
     private JButton Sortir;
-    private JTextPane textPane1;
     private JFilePicker Picker1;
     private JFilePicker Picker2;
     private JRadioButton LZW;
     private JRadioButton LZSS;
     private JRadioButton LZ78;
     private JRadioButton JPEG;
-    private JRadioButton Fitxer;
-    private JRadioButton Carpeta;
-    private JRadioButton Compress;
-    private JRadioButton Descompress;
-    private JButton Action;
     private JButton Compare;
+    private JButton Globales;
+    private JButton Accion;
+    private JSlider slider1;
+    private JPanel Auxiliar;
     private static JFrame frame;
-    /* 0 : LZW
-    *  1 : LZSS
-    *  2 : LZ78
-    *  3 : JPEG*/
-    private static int state = 0;
-    /* 0 : File
-    *  1 : Directory*/
     private static int metodo = 0;
-    /* 0 : Compression
-    *  1 : Descompression*/
-    private static int compresion = 0;
-    private static String pos = ".txt";
-    private static String Sec = "TXT Files";
+    private boolean directorio = false;
     private JRadioButton [] L = new JRadioButton[] {LZW, LZSS, LZ78, JPEG};
-    private JRadioButton [] J = new JRadioButton[] {Fitxer, Carpeta};
-    private JRadioButton [] K = new JRadioButton[] {Compress, Descompress};
     HashMap<Integer, String> M = new HashMap<Integer, String>();
 
     private void createUIComponents () {
-        Picker1 = new JFilePicker("Selecció", "Busca");
-        Picker1.selectmode(-1);
+        Picker1 = new JFilePicker("Elemento a Comprimir/Descomprimir", "Busca");
         Picker1.setMode(JFilePicker.MODE_SAVE);
+        Picker1.getFileChooser().setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        String [] ext = new String[] {".txt", ".ppm", ".fw", ".fs", ".f8", ".fg", ".FW", ".FS", ".F8"};
+        Picker1.addFileTypeFilter(ext, "Archivo");
 
-        Picker2 = new JFilePicker("Selecció", "Busca");
-        Picker2.selectmode(1);
+        Picker2 = new JFilePicker("Seleccione destino", "Busca");
         Picker2.setMode(JFilePicker.MODE_SAVE);
+        Picker2.getFileChooser().setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 
-        if (state == 0)
-            Picker1.addFileTypeFilter(pos, Sec);
     }
 
-    public void changeFilter () {
-        if (metodo == 3) {
-            if (compresion == 0) {
-                pos = ".ppm";
-                Sec = "PPM Files";
-            }
-            else  {
-                if (state == 0) {
-                    pos = ".fG";
-                    Sec = "PPM Compressed-Files";
-                }
-                else {
-                    pos = ".FG";
-                    Sec = "PPM Compressed-Folder";
-                }
-            }
-        } else {
-            if (compresion == 0) {
-                pos = ".txt";
-                Sec = "TXT Files";
-            }
-            else {
-                if (state == 0) {
-                    if (metodo == 0) pos = ".fW";
-                    else if (metodo == 1) pos = ".fS";
-                    else if (metodo == 2) pos = ".f8";
-                    else pos = ".fG";
-                    Sec = "TXT Compressed File";
-                }
-                else {
-                    if (metodo == 0) pos = ".FW";
-                    else if (metodo == 1) pos = ".FS";
-                    else if (metodo == 2) pos = ".F8";
-                    else pos = ".FG";
-                    Sec = "TXT Compressed Folder";
-                }
-            }
+    class ResizeListener extends ComponentAdapter {
+        public void componentResized(ComponentEvent e) {
+            // Recalculate the variable you mentioned
+            Picker1.gettextf().setColumns(frame.getSize().width/20);
+            Picker2.gettextf().setColumns(frame.getSize().width/20);
         }
-        Picker1.addFileTypeFilter(pos,Sec);
     }
 
-    public void actPickers() {
-        Picker1.selectmode(-1);
-        Picker1.updateUI();
-        Picker1.removefilter();
-        changeFilter();
+
+    public class Framepop extends JFrame {
+        JFrame Frame = new JFrame();
+        TextField tf1 = new TextField();
+        TextField tf2 = new TextField();
+
+        public Framepop (String s1, String s2) {
+            super("Comparacion");
+            Frame.setEnabled(true);
+            Frame.setVisible(true);
+            Frame.setSize(300, 300);
+            tf1.setEnabled(true);
+            tf2.setEnabled(true);
+            tf1.setEditable(false);
+            tf2.setEditable(false);
+            tf1.setVisible(true);
+            tf2.setVisible(true);
+            tf1.setText(s1);
+            tf2.setText(s2);
+            Frame.add(tf1);
+            Frame.add(tf2);
+            Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
     }
+
 
     public Interfaz() {
+        frame.setSize(40, 40);
+        frame.addComponentListener(new ResizeListener());
+        Picker1.gettextf().setColumns(frame.getSize().width);
+        Picker2.gettextf().setColumns(frame.getSize().width);
+        slider1.setPaintTicks(true);
+        slider1.setSnapToTicks(true);
+        slider1.setVisible(false);
+        slider1.setMaximumSize(new Dimension(0,0));
+        Accion.setText("Comprimir/Descomprimir");
         M.put(0, "LZW");
         M.put(1, "LZSS");
         M.put(2, "LZ78");
@@ -240,15 +225,7 @@ public class Interfaz extends JFrame {
         group1.add(LZW);
         group1.add(LZSS);
         group1.add(JPEG);
-        ButtonGroup group2 = new ButtonGroup();
-        group2.add(Compress);
-        group2.add(Descompress);
-        ButtonGroup group3 = new ButtonGroup();
-        group3.add(Fitxer);
-        group3.add(Carpeta);
-        if (!L[metodo].isSelected()) L[metodo].doClick();
-        if (!J[state].isSelected()) J[state].doClick();
-        if (!K[compresion].isSelected()) K[compresion].doClick();
+        for (JRadioButton b : L) b.setEnabled(false);
         Sortir.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -256,27 +233,80 @@ public class Interfaz extends JFrame {
             }
         });
 
-        Action.addActionListener(new ActionListener() {
+        Picker1.textField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                act();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                //act();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                act();
+            }
+
+            public void act () {
+                String p = Picker1.getSelectedFilePath();
+                File f = new File(p);
+                directorio = f.isDirectory();
+                if (directorio) {
+                    Accion.setText("Comprimir");
+                    for (JRadioButton b : L) b.setEnabled(true);
+                    L[metodo].doClick();
+                    JPEG.setEnabled(false);
+                    if (M.get(metodo).equals("JPEG")) {
+                        metodo = 0;
+                        L[metodo].doClick();
+                    }
+                }
+                else {
+                    if (p.substring(p.length()-4).equals(".txt") && !p.substring(p.length()-4).equals(".ppm")) {
+                        Accion.setText("Comprimir");
+                        for (JRadioButton b : L) b.setEnabled(true);
+                        JPEG.setEnabled(false);
+                        L[metodo].doClick();
+                    }
+                    else if (p.substring(p.length()-4).equals(".txt") || p.substring(p.length()-4).equals(".ppm")) {
+                        Accion.setText("Comprimir");
+                        for (JRadioButton b : L) b.setEnabled(true);
+                        L[metodo].doClick();
+                    }
+                    else {
+                        Accion.setText("Descomprimir");
+                        group1.clearSelection();
+                        for (JRadioButton b : L) b.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+        Accion.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Cont_CD C = new Cont_CD();
-                try {
-                    if (compresion == 0) {
-                        if (state == 0)
-                            C.compressio_fitxer(Picker1.getSelectedFilePath(), Picker2.getSelectedFilePath(), M.get(metodo));
-                        else
+                if (Accion.getText().equals("Comprimir/Descomprimir")) {
+                    JOptionPane.showMessageDialog(frame, "Escoge antes un Elemento a comprimir o descomprimir");
+                } else {
+                    try {
+                        if (!directorio)
+                            if (Accion.getText().equals("Comprimir"))
+                                C.compressio_fitxer(Picker1.getSelectedFilePath(), Picker2.getSelectedFilePath(), M.get(metodo));
+                            else
+                                C.descompressio_fitxer(Picker1.getSelectedFilePath(), Picker2.getSelectedFilePath());
+                        else if (Accion.getText().equals("Comprimir"))
                             C.compressio_carpeta(Picker1.getSelectedFilePath(), Picker2.getSelectedFilePath(), M.get(metodo));
-                    } else {
-                        if (state == 0)
-                            C.descompressio_fitxer(Picker1.getSelectedFilePath(), Picker2.getSelectedFilePath());
                         else
                             C.descompressio_carpeta(Picker1.getSelectedFilePath(), Picker2.getSelectedFilePath());
-
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    } catch (controlador_gestor_fitxer.FicheroCompressionNoValido | controlador_gestor_fitxer.FicheroDescompressionNoValido ficheroCompressionNoValido) {
+                        JOptionPane.showMessageDialog(frame, ficheroCompressionNoValido.getMessage());
                     }
-                } catch (IOException ex) {
-                    ex.printStackTrace();
                 }
-
             }
         });
 
@@ -286,52 +316,27 @@ public class Interfaz extends JFrame {
                 Cont_CD C = new Cont_CD();
                 try {
                     String[] S = C.comparar();
+
+                    Framepop p = new Framepop(S[0], S[1]);
+                    p.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    p.setSize(300, 300);
+
                 } catch (IOException ex) {
                     ex.printStackTrace();
+                } catch (controlador_gestor_fitxer.FicheroCompressionNoValido | controlador_gestor_fitxer.FicheroDescompressionNoValido ficheroCompressionNoValido) {
+                    JOptionPane.showMessageDialog(frame, ficheroCompressionNoValido.getMessage());
                 }
             }
         });
 
-        Carpeta.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                state = 1;
-                actPickers();
-                //frame.setContentPane(new Interfaz().Panel);
-            }
-        });
 
-        Fitxer.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                state = 0;
-                actPickers();
-                //frame.setContentPane(new Interfaz().Panel);
-
-            }
-        });
-
-        Descompress.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                compresion = 1;
-                actPickers();
-            }
-        });
-
-        Compress.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                compresion = 0;
-                actPickers();
-            }
-        });
 
         LZW.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 metodo = 0;
-                actPickers();
+                slider1.setVisible(false);
+                slider1.setMaximumSize(new Dimension(0,0));
             }
         });
 
@@ -339,7 +344,8 @@ public class Interfaz extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 metodo = 1;
-                actPickers();
+                slider1.setVisible(false);
+                slider1.setMaximumSize(new Dimension(0,0));
             }
         });
 
@@ -347,7 +353,8 @@ public class Interfaz extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 metodo = 2;
-                actPickers();
+                slider1.setVisible(false);
+                slider1.setMaximumSize(new Dimension(0,0));
             }
         });
 
@@ -355,7 +362,8 @@ public class Interfaz extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 metodo = 3;
-                actPickers();
+                slider1.setVisible(true);
+                slider1.setMaximumSize(new Dimension(30,50));
             }
         });
 
@@ -366,6 +374,9 @@ public class Interfaz extends JFrame {
         frame.setContentPane(new Interfaz().Panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+        frame.setSize(900, 300);
+        frame.setMaximumSize(new Dimension(900, 300));
+        frame.setMinimumSize(new Dimension(900, 300));
         frame.setVisible(true);
     }
 
