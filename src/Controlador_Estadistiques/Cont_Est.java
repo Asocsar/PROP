0d6fa51faaa496726a657db9fe5873c3e1eff48e
Clums98 <56516776+Clums98@users.noreply.github.com>
2019-12-19 20:10:37 +0100

    Add files via upload

--- src/Controlador_Estadistiques/Cont_Est.java
+++ src/Controlador_Estadistiques/Cont_Est.java
@@ -4,13 +4,20 @@ import Estadístiques.Estadistiques;
 import java.io.*;
 import java.nio.file.FileAlreadyExistsException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
 
 
 public class Cont_Est {
 
-
+    /*public static void main (String[] args) throws IOException {
+        GetStats();
+        Stats_Update();
+        Map<String,List<Double>> temp = new HashMap<>();
+        temp = Estadistiques.getparam();
+        System.out.print(temp.values());
+    }*/
 
     /** \brief Actualitza l'arxiu d'estadístiques
      \pre Cert
@@ -18,59 +25,59 @@ public class Cont_Est {
      \throw IOException
      */
 
-    public void Stats_Update() throws  IOException {
+    public static void Stats_Update() throws IOException {
 
         //Update stats from last compression
+         Estadistiques E = new Estadistiques();
+         File out = new File("Estadisticas.txt");
+         if (!out.exists()) out.createNewFile();
+         PrintWriter pw = new PrintWriter(new FileWriter(out));
+         pw.print("");
+         pw.close();
+         pw = new PrintWriter(new FileWriter(out));
 
-        Estadistiques E = new Estadistiques();
-        File out = new File("Estadisticas.txt");
-        if (!out.exists()) out.createNewFile();
-        PrintWriter pw = new PrintWriter(new FileWriter(out));
-        pw.print("");
-        pw.close();
-        pw = new PrintWriter(new FileWriter(out));
+         List<Double> actalg = new ArrayList<>();
 
-        List<Double>  actalg = new ArrayList<>();
+         // Save Stats LZW
 
-        // Save Stats LZW
-
-        actalg = Estadistiques.act(E.getTimeLZW(),E.getGlobTimeLZW(),E.getDTimeLZW(),E.getDGlobTimeLZW(),E.getRatioLZW(),E.getGlobRatioLZW(),E.getVelLZW(),E.getQuantLZW(),E.getDQuantLZW());
-        E.setLZW(actalg);
-
-
-        pw.print(0+"," + actalg.get(0) +","+ actalg.get(1) +","+ actalg.get(2) + ","+ actalg.get(3) +","+ actalg.get(4) + "," + actalg.get(5) + "," + actalg.get(6) + "," + actalg.get(7) + "," + actalg.get(8) + "\n");
-
-        // Save Stats LZSS
-
-        actalg = Estadistiques.act(E.getTimeLZSS(),E.getGlobTimeLZSS(),E.getDTimeLZSS(),E.getDGlobTimeLZSS(),E.getRatioLZSS(),E.getGlobRatioLZSS(),E.getVelLZSS(),E.getQuantLZSS(),E.getDQuantLZSS());
-        E.setLZSS(actalg);
+         actalg = Estadistiques.getstatsLZW();//act(E.getTimeLZW(), E.getGlobTimeLZW(), E.getDTimeLZW(), E.getDGlobTimeLZW(), E.getRatioLZW(), E.getGlobRatioLZW(), E.getVelLZW(), E.getQuantLZW(), E.getDQuantLZW());
+         E.setLZW(actalg);
 
 
-        pw.print(1+"," + actalg.get(0) +","+ actalg.get(1) +","+ actalg.get(2) + ","+ actalg.get(3) +","+ actalg.get(4) + "," + actalg.get(5) + "," + actalg.get(6) + "," + actalg.get(7) + "," + actalg.get(8) + "\n");
+         pw.print(0 + "," + actalg.get(0) + "," + actalg.get(1) + "," + actalg.get(2) + "," + actalg.get(3) + "," + actalg.get(4) + "," + actalg.get(5) + "," + actalg.get(6) + "," + actalg.get(7) + "," + actalg.get(8) + "\n");
+
+         // Save Stats LZSS
+
+         actalg = Estadistiques.getstatsLZSS();//act(E.getTimeLZSS(), E.getGlobTimeLZSS(), E.getDTimeLZSS(), E.getDGlobTimeLZSS(), E.getRatioLZSS(), E.getGlobRatioLZSS(), E.getVelLZSS(), E.getQuantLZSS(), E.getDQuantLZSS());
+         E.setLZSS(actalg);
 
 
-        // Save Stats LZ78
-
-        actalg = Estadistiques.act(E.getTimeLZ78(),E.getGlobTimeLZ78(),E.getDTimeLZ78(),E.getDGlobTimeLZ78(),E.getRatioLZ78(),E.getGlobRatioLZ78(),E.getVelLZ78(),E.getQuantLZ78(),E.getDQuantLZ78());
-        E.setLZ78(actalg);
-
-        pw.print(2+"," + actalg.get(0) +","+ actalg.get(1) +","+ actalg.get(2) + ","+ actalg.get(3) +","+ actalg.get(4) + "," + actalg.get(5) + "," + actalg.get(6) + "," + actalg.get(7) + "," + actalg.get(8) + "\n");
+         pw.print(1 + "," + actalg.get(0) + "," + actalg.get(1) + "," + actalg.get(2) + "," + actalg.get(3) + "," + actalg.get(4) + "," + actalg.get(5) + "," + actalg.get(6) + "," + actalg.get(7) + "," + actalg.get(8) + "\n");
 
 
-        // Save Stats JPEG
+         // Save Stats LZ78
 
-        actalg = Estadistiques.act(E.getTimeJPEG(),E.getGlobTimeJPEG(),E.getDTimeJPEG(),E.getDGlobTimeJPEG(),E.getRatioJPEG(),E.getGlobRatioJPEG(),E.getVelJPEG(),E.getQuantJPEG(),E.getDQuantJPEG());
-        E.setJPEG(actalg);
+         actalg = Estadistiques.getstatsLZ78();//act(E.getTimeLZ78(), E.getGlobTimeLZ78(), E.getDTimeLZ78(), E.getDGlobTimeLZ78(), E.getRatioLZ78(), E.getGlobRatioLZ78(), E.getVelLZ78(), E.getQuantLZ78(), E.getDQuantLZ78());
+         E.setLZ78(actalg);
 
-        pw.print(3+"," + actalg.get(0) +","+ actalg.get(1) +","+ actalg.get(2) + ","+ actalg.get(3) +","+ actalg.get(4) + "," + actalg.get(5) + "," + actalg.get(6) + "," + actalg.get(7) + "," + actalg.get(8) + "\n");
+         pw.print(2 + "," + actalg.get(0) + "," + actalg.get(1) + "," + actalg.get(2) + "," + actalg.get(3) + "," + actalg.get(4) + "," + actalg.get(5) + "," + actalg.get(6) + "," + actalg.get(7) + "," + actalg.get(8) + "\n");
 
-        //Save Last Algorithm
 
-        E.setLastAlg(E.getLastAlg());
+         // Save Stats JPEG
 
-        pw.print(4+","+E.getLastAlg());
+         actalg = Estadistiques.getstatsJPEG();//act(E.getTimeJPEG(), E.getGlobTimeJPEG(), E.getDTimeJPEG(), E.getDGlobTimeJPEG(), E.getRatioJPEG(), E.getGlobRatioJPEG(), E.getVelJPEG(), E.getQuantJPEG(), E.getDQuantJPEG());
+         E.setJPEG(actalg);
 
-        pw.close();
+         pw.print(3 + "," + actalg.get(0) + "," + actalg.get(1) + "," + actalg.get(2) + "," + actalg.get(3) + "," + actalg.get(4) + "," + actalg.get(5) + "," + actalg.get(6) + "," + actalg.get(7) + "," + actalg.get(8) + "\n");
+
+         //Save Last Algorithm
+
+         E.setLastAlg(E.getLastAlg());
+
+         pw.print(4 + "," + E.getLastAlg());
+
+         pw.close();
+
 
     }
 
@@ -82,7 +89,7 @@ public class Cont_Est {
      */
 
 
-    public void GetStats () throws IOException{ //Set stats from file to classes
+    public static void GetStats() throws IOException{ //Set stats from file to classes
 
 
         File OldStats = new File("Estadisticas.txt");
@@ -113,7 +120,7 @@ public class Cont_Est {
         String line = "";
         int id;
         while ((act = file.read()) != -1) {
-            act = Double.valueOf(Character.toString((char) act));
+            act = Double.parseDouble(Character.toString((char) act));
             if (act == 0 | act == 1 | act == 2 | act == 3 | act == 4) {
                 id = (int)act;
                 line = file.readLine();
